24e97810da2380bb0dc4224097fbd3bcf0afc8fe
Miguel-ectil <ectilmiguelmiguelectil.com>
2025-06-07 23:03:43 -0300

    [ADD] Adicionando importações e o código segunda parte do projeto

--- projeto_rpa.py
+++ projeto_rpa.py
@@ -1,5 +1,8 @@
 import requests
-
+import sqlite3
+import re
+import yagmail
+import datetime
 
 API_KEY = 'SUA_CHAVE_API_AQUI'
 CIDADE = 'Guarulhos'
@@ -15,3 +18,27 @@ data = response.json()
 temperatura = data['main']['temp']
 descricao = data['weather'][0]['description']
 umidade = data['main']['humidity']
+
+# === ETAPA 2: Armazenamento no Banco de Dados ===
+conn = sqlite3.connect('projeto_rpa.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS dados_climaticos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cidade TEXT,
+    temperatura REAL,
+    descricao TEXT,
+    umidade INTEGER,
+    data_hora TEXT
+)
+''')
+
+data_hora = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+cursor.execute('''
+INSERT INTO dados_climaticos (cidade, temperatura, descricao, umidade, data_hora)
+VALUES (?, ?, ?, ?, ?)
+''', (CIDADE, temperatura, descricao, umidade, data_hora))
+conn.commit()
+
+
